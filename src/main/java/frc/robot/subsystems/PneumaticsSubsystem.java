94ee80e2ad6d9e7e7acb3b6d7da9c666787de63b
Johnathan Sims <johnathan.sims360@gmail.com>
2023-01-28 15:02:21 -0700

    Added constant for PCM ID

--- src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
+++ src/main/java/frc/robot/subsystems/PneumaticsSubsystem.java
@@ -7,11 +7,10 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj.Compressor;
 import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class PneumaticsSubsystem extends SubsystemBase {
-
-  //Creates a new compressor on the CAN ID 0, as 0 is the default
-  private Compressor compressor = new Compressor(PneumaticsModuleType.CTREPCM);
+  private Compressor compressor = new Compressor(Constants.PneumaticsConstants.kPcmCanId, PneumaticsModuleType.CTREPCM);
 
   /** Creates a new PneumaticsSubsystem. */
   public PneumaticsSubsystem() {
