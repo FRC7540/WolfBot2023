8dc0d66a110a5bfdfc1a5d114eb2ec1a594e2902
Johnathan Sims <johnathan.sims360@gmail.com>
2023-01-31 19:15:07 -0700

    Added shuffleboard entry and listener to disable compressor

---
+++ src/main/java/frc/robot/commands/SetCompressor.java
@@ -0,0 +1,35 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.networktables.NetworkTableEvent;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import frc.robot.subsystems.PneumaticsSubsystem;
+
+public class SetCompressor extends CommandBase {
+  private PneumaticsSubsystem pneumaticsSubsystem;
+  private Boolean enabled;
+
+  /** Creates a new SetCompressor. */
+  public SetCompressor(PneumaticsSubsystem pneumaticsSubsystem, NetworkTableEvent enabled) {
+    this.pneumaticsSubsystem = pneumaticsSubsystem;
+    this.enabled = enabled.valueData.value.getBoolean();
+    addRequirements(pneumaticsSubsystem);
+  }
+
+  @Override
+  public void initialize() {
+    if (enabled) {
+      pneumaticsSubsystem.EnableCompressor();
+    } else {
+      pneumaticsSubsystem.DisableCompressor();
+    }
+  }
+
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
