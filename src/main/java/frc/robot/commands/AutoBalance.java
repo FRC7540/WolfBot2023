a019b0c620819306077e99f264525cbdccce0c94
Ewan Kannolt <ekannolt@eastmountainhigh.net>
2023-02-23 19:22:21 -0700

    fixed auto balance by getting rid of unnecessary function

--- src/main/java/frc/robot/commands/AutoBalance.java
+++ src/main/java/frc/robot/commands/AutoBalance.java
@@ -47,11 +47,6 @@ public class AutoBalance extends PIDCommand {
     drivebaseSubsystem.setFieldOrientedDriveEnabled(false);
   }
 
-  // Called every time the scheduler runs while the command is scheduled.
-  @Override
-  public void execute() {
-  }
-
   // Called once the command ends or is interrupted.
   @Override
   public void end(boolean interrupted) {
