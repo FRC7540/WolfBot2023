d1c52e14866dd04dae2e16a9273f4577f06801cf
TheInatorInator <Logan.Mamanakis@gmail.com>
2023-04-01 14:00:06 -0600

    add prints to auto

--- src/main/java/frc/robot/commands/AutoPlaceCone.java
+++ src/main/java/frc/robot/commands/AutoPlaceCone.java
@@ -1,6 +1,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.InstantCommand;
+import edu.wpi.first.wpilibj2.command.PrintCommand;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.Dashboard;
@@ -16,14 +17,17 @@ public class AutoPlaceCone extends SequentialCommandGroup {
             CraneSubsystem craneSubsystem) {
                 OperateCrane operateCrane = new OperateCrane(craneSubsystem, () -> 0.0);
         addCommands(
+                new PrintCommand("Starting Cone Place"),
+
                 new InstantCommand(() -> drivebaseSubsystem.resetNav(), drivebaseSubsystem),
                 // Stage 1: place a cone, rotate
 
                 // raise arm
-                new InstantCommand(() -> operateCrane.recallPreset(armPreset.UPPER_NODE), craneSubsystem),
+                //new InstantCommand(() -> operateCrane.recallPreset(armPreset.UPPER_NODE), craneSubsystem),
 
                 new WaitCommand(Dashboard.autonomousWaitToRaise.get().getDouble()),
 
+                new PrintCommand("Driving forward"),
                 // drive up
                 new LockedDrive(drivebaseSubsystem, () -> 0.0, () -> Dashboard.autonomousDriveToPlaceSpeed.get().getDouble(),
                          () -> true, Dashboard.rotationController)
@@ -32,6 +36,8 @@ public class AutoPlaceCone extends SequentialCommandGroup {
                         .withTimeout(0.5),
 
                 // open claw
+                new PrintCommand("Drop cone"),
+
                 new WaitCommand(Dashboard.autonomousWaitToPlaceDelay.get().getDouble()),
 
                 new ActuateClaw(clawSubsystem, Direction.BACKWARD),
@@ -39,6 +45,9 @@ public class AutoPlaceCone extends SequentialCommandGroup {
                 new WaitCommand(Dashboard.autonomousWaitAfterPlaceDelay.get().getDouble()),
 
                 // drive back
+
+                new PrintCommand("Drive back"),
+
                 new InstantCommand(() -> drivebaseSubsystem.resetNav(), drivebaseSubsystem),
                 new LockedDrive(drivebaseSubsystem, () -> 0.0, () -> Dashboard.autonomousReverseFromPlaceSpeed.get().getDouble(),
                         () -> true, Dashboard.rotationController)
@@ -46,11 +55,15 @@ public class AutoPlaceCone extends SequentialCommandGroup {
                                 .getDisplacementY() <= Dashboard.autonomousReverseFromPlaceDistance.get().getDouble()),
 
                 // lower crane
+                new PrintCommand("Lower crane"),
+
                 new InstantCommand(() -> operateCrane.recallPreset(armPreset.HOME), craneSubsystem),
 
                 new WaitCommand(Dashboard.autonomousWaitToLower.get().getDouble()),
 
                 // rotate 180
+                new PrintCommand("Rotate 180"),
+
                 new InstantCommand(() -> drivebaseSubsystem.resetNav(), drivebaseSubsystem),
                 new Drive(drivebaseSubsystem, () -> 0.0, () -> 0.0,
                         () -> Dashboard.autonomousRotateSpeed.get().getDouble(), () -> true)
